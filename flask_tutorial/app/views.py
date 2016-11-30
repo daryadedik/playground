1cc27e16640a9831a49bfe841579456ee9566402
Darya Dedik <darya.dedik@gmail.com>
2016-11-30 22:33:27 +0100

    added a name in the flask tutorial

--- flask_tutorial/app/views.py
+++ flask_tutorial/app/views.py
@@ -1,7 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
-
+    user = {'nickname': 'Miguel'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
