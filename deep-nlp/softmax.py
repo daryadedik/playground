3e3033b5bc4d23eeffc4df5da11702e262424af1
Darya Dedik <daryadedik@gmail.com>
2017-01-20 15:58:56 +0000

    computing softmax

---
+++ deep-nlp/softmax.py
@@ -0,0 +1,35 @@
+# Initial code taken from assignments https://cs224d.stanford.edu
+
+import numpy as np
+import random
+
+def softmax(x):
+    
+    x = np.exp(x) / np.sum(np.exp(x))
+
+    return x
+
+def test_softmax_basic():
+    
+    print "Running basic tests..."
+    test1 = softmax(np.array([1,2]))
+    print test1
+    assert np.amax(np.fabs(test1 - np.array(
+        [0.26894142,  0.73105858]))) <= 1e-6
+
+    
+    test2 = softmax(np.array([[1001,1002],[3,4]]))
+    print test2
+    assert np.amax(np.fabs(test2 - np.array(
+        [[0.26894142, 0.73105858], [0.26894142, 0.73105858]]))) <= 1e-6
+    
+    
+    test3 = softmax(np.array([[-1001,-1002]]))
+    print test3
+    assert np.amax(np.fabs(test3 - np.array(
+        [0.73105858, 0.26894142]))) <= 1e-6
+
+    print "You should verify these results!\n"
+    
+if __name__ == "__main__":
+    test_softmax_basic()
